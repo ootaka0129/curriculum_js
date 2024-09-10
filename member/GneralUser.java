300c124fa87aa2ae69440e3f2a6355aefe76c503
ootaka0129 <yuutaka0129@icloud.com>
2024-09-10 10:52:27 +0900

    理解度問題

---
+++ member/GneralUser.java
@@ -0,0 +1,12 @@
+package member;
+
+public class GeneralUser extends User {
+    //初期化
+    public 
+    
+    @Override
+    public String toString() {
+        return "ユーザー名：" + name + "　メールアドレス：" + mail;
+    }
+        
+}
---
+++ member/Main.java
@@ -0,0 +1,64 @@
+package member;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private static List<User> users = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean keepRunning = true;
+
+        while (keepRunning) {  //trueの間繰り返す
+            System.out.println("1. 一般ユーザーを登録");
+            System.out.println("2. 管理者ユーザーを登録");
+            System.out.println("3. ユーザー情報を表示");
+            System.out.println("4. 終了");
+            System.out.print("選択してください: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();  // 改行文字を消費
+
+            switch (choice) {
+                case 1:
+                    registerGeneralUser(scanner);
+                    break;
+                case 2:
+                    registerAdminUser(scanner);
+                    break;
+                case 3:
+                    findUsers();
+                    break;
+                case 4:
+                    keepRunning = false;
+                    break;
+                default:
+                    System.out.println("無効な選択です。");
+            }
+        }
+
+        scanner.close();
+    }
+
+    // 一般ユーザーを登録するメソッド(名前とメール）
+    public static void registerGeneralUser(Scanner scanner) {
+        
+    }
+    // 管理者ユーザーを登録するメソッド(名前とメールと管理者コード)
+    public static void registerAdminUser(Scanner scanner) {
+        
+    }
+    // メールアドレスが既に登録されているか確認するメソッド
+    public static void isEmailRegistered() {
+        
+    }
+    // 登録されたユーザーを表示するメソッド
+    public static void findUsers() {
+        
+    }
+    
+    
+}
+
+
