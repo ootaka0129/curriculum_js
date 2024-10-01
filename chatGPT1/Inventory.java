32e34d2a0bae67ce00545d706638a4dd89c71891
ootaka0129 <yuutaka0129@icloud.com>
2024-10-01 16:20:27 +0900

    chatGPT問題について

--- chatGPT1/Inventory.java
+++ chatGPT1/Inventory.java
@@ -2,21 +2,28 @@ package chatGPT1;
 
 public class Inventory {
 
-    List<Product> p = new ArrayList<Product>();
-    
-    //商品を追加するためのメソッド 
-    public void addProduct(Product product){
-        p.add(product);
-        System.out.println(product + "を追加しました。")
+    List<Product> productList = new ArrayList<Product>();
+
+    //在庫数を更新するメソッド voidで戻り値なし
+    public void updateInventory(int amount){
+        if(getStock() + amount <= 0){
+            System.out.println("在庫数が０以下になっています。");
+            return;
+        }else if(getStock() + amount >= 0){
+            int updateStock = getStock() += amount;
+            System.out.println("現在の在庫は、" +  updateStock + "です。");
+        }
     }
+
     //特定の商品名で商品を検索するためのメソッド
     //検索するときは一つ一つ出力した後に、名前の一致を確かめる
-    public findProductByName(String name){
-        for(Product product : p){
+    public Product findProductByName(String name){
+        for(Product product : productList){
             if(product.getName().equals(name)){
                 return product;
             }
-            return null;
         }
+        return null;
     }
+
 }
--- chatGPT1/Product.java
+++ chatGPT1/Product.java
@@ -23,25 +23,16 @@ public class Product {
     public int getStock(){
         return stock;
     }
-
-    //在庫数を更新するメソッド voidで戻り値なし
-    public void updateStock(int amount){
-        if(stock + amount <= 0){
-            System.out.println("在庫数が０以下になっています。");
-            break;
-        }
-        if(stock + amount >= 0){
-            stock += amount;
-            System.out.println("現在の在庫は、" + stock + "です。");
-        }
-    }
-    
     //staticメソッドがあればインスタンス化しなくてもアクセスできる
     //voidは、戻り値がないことを示す。
     //static void インスタンス化不要の戻り値のないメソッド
-
     public String toString(){
         return "商品名:" + name + "価格:" + price + "在庫数:" + stock;
+    }
+
+    // 商品を追加するメソッド
+    public void addProduct(Product product){
+        productList.add(Product);
     }
 
 }
