ed46ced6b55d67d4dd290c5c3f49ad9f9e839729
ootaka0129 <yuutaka0129@icloud.com>
2024-12-02 22:01:54 +0900

    Delete ginnkou directory

--- ginnkou/src/Test/BankException.java
+++
@@ -1,8 +0,0 @@
-package Test;
-//Excepotion 独自のExcepotionを継承している
-public class BankException extends Exception {
-    public BankException(String message) {
-        super(message);
-    }
-    //親クラスのコンストラクタを呼びだすときにsuperを使用する
-}
